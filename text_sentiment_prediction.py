2524bba47c6df443b5b13e0a3a91194a43193404
PreetiSharma15 <pwhitehat6@gmail.com>
2022-01-28 14:48:46 +0530

    Initial Commit

---
+++ text_sentiment_prediction.py
@@ -0,0 +1,104 @@
+import pandas as pd
+import numpy as np
+
+import tensorflow.keras
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+
+
+train_data = pd.read_csv("./static/assets/data_files/tweet_emotions.csv")   
+
+training_sentences = []
+
+for i in range(len(train_data)):
+    sentence = train_data.loc[i, "content"]
+    training_sentences.append(sentence)
+
+model = load_model("./static/assets/model_files/Text_Emotion.h5")
+
+vocab_size = 40000
+max_length = 100
+trunc_type = "post"
+padding_type = "post"
+oov_tok = "<OOV>"
+
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(training_sentences)
+
+emo_code_url = {
+    "anger": [0, "./static/assets/emoticons/Anger.png"],
+    "fear": [1,"./static/assets/emoticons/Fear.png" ],
+    "joy": [2, "./static/assets/emoticons/Joy.png"],
+    "love": [3, "./static/assets/emoticons/Love.png"],
+    "sadness": [4, "./static/assets/emoticons/Sadness.png"],
+    "surprise": [5, "./static/assets/emoticons/Surprise.png"]
+    }
+
+def predict(text):
+
+    predicted_emotion=""
+    predicted_emotion_img_url=""
+    
+    if  text!="":
+        sentence = []
+        sentence.append(text)
+
+        sequences = tokenizer.texts_to_sequences(sentence)
+
+        padded = pad_sequences(
+            sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type
+        )
+        testing_padded = np.array(padded)
+
+        predicted_class_label = np.argmax(model.predict(testing_padded), axis=-1)        
+            
+        for key, value in emo_code_url.items():
+            if value[0]==predicted_class_label:
+                predicted_emotion_img_url=value[1]
+                predicted_emotion=key
+        return predicted_emotion, predicted_emotion_img_url
+
+
+
+def show_entry():
+    day_entry_list = pd.read_csv("./static/assets/data_files/data_entry.csv")
+
+    day_entry_list = day_entry_list.iloc[::-1]
+    
+    date1, date2, date3 =  pd.to_datetime(day_entry_list['date'].values[0]), pd.to_datetime(day_entry_list['date'].values[1]), pd.to_datetime(day_entry_list['date'].values[2])
+    entry1, entry2, entry3 = day_entry_list['text'].values[0], day_entry_list['text'].values[1], day_entry_list['text'].values[2]
+    emotion1, emotion2, emotion3 = day_entry_list["emotion"].values[0], day_entry_list["emotion"].values[1], day_entry_list["emotion"].values[2]
+
+    emotion_url_1=""
+    emotion_url_2=""
+    emotion_url_3=""
+
+    for key, value in emo_code_url.items():
+        if key==emotion1:
+            emotion_url_1 = value[1]
+        if key==emotion2:
+            emotion_url_2 = value[1]
+        if key==emotion3:
+            emotion_url_3 = value[1]
+
+    return [
+        {
+            "date": date1.strftime("%A, %d %B %Y"),
+            "entry": entry1,
+            "emotion": emotion1,
+            "emotion_url": emotion_url_1
+        },
+        {
+            "date": date2.strftime("%A, %d %B %Y"),
+            "entry": entry2,
+            "emotion": emotion2,
+            "emotion_url": emotion_url_2
+        },
+        {
+            "date": date3.strftime("%A, %d %B %Y"),
+            "entry": entry3,
+            "emotion": emotion3,
+            "emotion_url": emotion_url_3
+        }
+    ]
